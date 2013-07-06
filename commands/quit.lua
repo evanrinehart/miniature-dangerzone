980dadf4fab36a245cd448219f1afbf27381d5e9
EvanR <evanrinehart@gmail.com>
2013-07-06 11:43:14 -0500

    forgot the quit command file

---
+++ commands/quit.lua
@@ -0,0 +1,15 @@
+return {
+  effect = function(me, args)
+    if args.command == 'quit' then
+      tell(me, "ok")
+      c_kick(me.fd)
+      -- careful, no more messages may be sent to player
+      me.connected = false
+    else
+      tell(me, "please spell out the whole quit command to really quit")
+    end
+  end,
+  usage = "quit by itself, spelled out completel",
+  patterns = {'v'},
+  verbs = {'quit', 'q'}
+}
