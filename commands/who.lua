26f72ba78990d17ffa872a8cf59a1a6ba4f356e6
EvanR <evanrinehart@gmail.com>
2013-07-09 21:24:28 -0500

    who command

--- commands/who.lua
+++ commands/who.lua
@@ -3,22 +3,32 @@ require ('util/tabular')
 local function who(me, args)
   local cols = {
     {
-      width=60,
+      width=20,
       align='left'
     },
     {
-      divider='|',
-      width=10,
+      width=40,
       align='right'
     }
   }
 
-  local rows = {
-    {{text='abc'},{text='123'}},
-    'divider',
-    {{text='ooo'},{text='5', color='red'}},
-    {{text='1234567'},{text='33'}}
-  }
+  local rows = {}
+
+  for pl in players_iter() do
+    if pl.creature then
+      local cr = pl.creature
+      local kind, place = db_lookup_location(cr.location)
+      table.insert(rows, {
+        {
+          text=cr:name(),
+          color=cr.color
+        },
+        {
+          text=place.name
+        }
+      })
+    end
+  end
 
   local data = tabular(rows, cols)
 
